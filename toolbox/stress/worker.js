d3797c108fc1a12e4d43a9b14528ca5d89d9e985
lideming <14901890+lideming@users.noreply.github.com>
2022-01-14 06:11:56 +0800

    update toolbox/stress

--- toolbox/stress/worker.js
+++ toolbox/stress/worker.js
@@ -1,7 +1,40 @@
-let counter = 0;
-while(1) {
-    if (++counter >= 1e8) { // 1 million
-        postMessage(1);
-        counter = 0;
+self.addEventListener('message', (e) => {
+    if (e.data.action == 'cpu') {
+        stressCpu();
+    } else if (e.data.action == 'mem') {
+        stressMem(e.data);
     }
+});
+
+postMessage('ready');
+
+function stressCpu() {
+    let counter = 0;
+    while (1) {
+        if (++counter >= 1e8) { // 1 million
+            postMessage('iteration');
+            counter = 0;
+        }
+    }
+}
+
+const memoryArray = [];
+
+function stressMem(data) {
+    const { size } = data;
+    for (let i = 0; i < size; i += 128) {
+        allocate(Math.min(128, size - i));
+    }
+}
+
+function allocate(size) {
+    const buffer = new Uint8Array(size * 1024 * 1024);
+    for (let i = 0; i < buffer.length; i++) {
+        buffer[i] = i % 256;
+    }
+    memoryArray.push(buffer);
+    postMessage({
+        action: 'mem',
+        size: size,
+    });
 }
